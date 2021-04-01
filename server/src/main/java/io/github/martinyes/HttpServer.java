b84a179d9106f92a8e876bf3b4cff6220ac77288
martinyes <martin@lexium.cloud>
2021-04-01 17:34:16 +0200

    added a few pages

--- server/src/main/java/io/github/martinyes/HttpServer.java
+++ server/src/main/java/io/github/martinyes/HttpServer.java
@@ -1,29 +1,59 @@
 package io.github.martinyes;
 
 import com.google.gson.JsonObject;
+import io.github.martinyes.page.Page;
 import io.github.martinyes.util.JsonBuilder;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import io.javalin.http.Handler;
 import lombok.Getter;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class HttpServer {
 
+    @Getter private static HttpServer instance;
     @Getter private static Javalin APP;
 
-    public static void main(String[] args) {
-        APP = Javalin.create().start(7000);
+    public HttpServer() {
+        instance = this;
+    }
 
-        APP.get("/test", ctx -> {
-            ctx.result(getResponseScheme(ctx).toString()).contentType("application/json");
+    public static void main(String[] args) {
+        new HttpServer().start();
+    }
+
+    public void start() {
+        APP = Javalin.create().start(7000);
+        APP.before("*", ctx -> ctx.contentType("application/json"));
+
+        addPage(new Page() {
+            @Override
+            public Handler handle() {
+                return ctx -> ctx.result(getResponseScheme(ctx).toString());
+            }
+
+            @Override
+            public String path() {
+                return "/get";
+            }
+        });
+        addPage(new Page() {
+            @Override
+            public Handler handle() {
+                return ctx -> ctx.redirect("/get?redirect=true");
+            }
+
+            @Override
+            public String path() {
+                return "/redirect";
+            }
         });
     }
 
-    public static JsonObject getResponseScheme(Context ctx) {
+    public JsonObject getResponseScheme(Context ctx) {
         JsonBuilder root = new JsonBuilder();
 
         JsonBuilder paramsTree = new JsonBuilder();
@@ -43,19 +73,45 @@ public class HttpServer {
             headerTree.add(entry.getKey(), entry.getValue());
 
         root.add("url", ctx.fullUrl());
-        root.add("http_ver", ctx.protocol());
+        root.add("protocol", ctx.protocol());
+        root.add("method", ctx.method());
+        root.add("redirect", (ctx.queryParam("redirect") != null));
         root.add("args", paramsTree.build());
         root.add("headers", headerTree.build());
 
         return root.build();
     }
 
-    private static JsonObject parseValueTree(List<String> values) {
+    private JsonObject parseValueTree(List<String> values) {
         JsonBuilder builder = new JsonBuilder();
         AtomicInteger index = new AtomicInteger();
 
         values.forEach(val -> builder.add(String.valueOf(index.getAndIncrement()), val));
 
         return builder.build();
+    }
+
+    private void addPage(Page page) {
+        switch (page.method()) {
+            case GET: {
+                APP.get(page.path(), page.handle());
+                break;
+            }
+
+            case POST: {
+                APP.post(page.path(), page.handle());
+                break;
+            }
+
+            case PUT: {
+                APP.put(page.path(), page.handle());
+                break;
+            }
+
+            case DELETE: {
+                APP.delete(page.path(), page.handle());
+                break;
+            }
+        }
     }
 }
